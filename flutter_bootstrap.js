78a0c9ffd94be7245fdc37a913d8c01728b018df
kristaloverbeer <kristaloverbeer@users.noreply.github.com>
2025-04-04 14:03:08 +0000

    deploy: 88837e70beae10a588895187f6233fec87c1d6aa

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "192013043"
+    serviceWorkerVersion: "3936392440"
   }
 });
